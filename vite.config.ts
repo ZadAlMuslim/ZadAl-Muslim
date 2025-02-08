a372863173a2c09b5508722413dd1173317011f6
AirSoft <105236641+ZadAlMuslim@users.noreply.github.com>
2025-02-08 13:18:46 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), runtimeErrorOverlay(), themePlugin()],
-  base: '/',
+  base: './',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client", "src"),
